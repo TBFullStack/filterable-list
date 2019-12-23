28f6b53cfb9cd630307b82baa6ec0b09e4c16701
longtermtobi <tbfullstack@gmail.com>
2019-12-23 12:30:30 +0000

    changed li to querySelectorAll and added loop to filter through contacts

--- app.js
+++ app.js
@@ -11,7 +11,17 @@ function filterNames() {
   // Get names <UL> get ID of it
   let ulNames = document.querySelector('#names');
 
-  //   Get list from <UL> query selector all
-  let li = document.querySelector('li.collection-item');
-  console.log(li);
+  //   Get list from <UL> querySelector of the ulNames above
+  let li = ulNames.querySelectorAll('li.collection-item');
+
+  // Loop through all collection-item li's
+  for (let i = 0; i < li.length; i++) {
+    let a = li[i].getElementsByTagName('a')[0];
+    // if matched
+    if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+      li[i].style.display = '';
+    } else {
+      li[i].style.display = 'none';
+    }
+  }
 }
